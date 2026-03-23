e0e09f5c4a8b60d6ec6e256c895179110a087144
Aluno Dendezeiros <guilherme.silva15@ba.estudante.senai.br>
2026-03-23 14:42:38 -0300

    Calculadora de idade para votar

---
+++ atividade_004/script.js
@@ -0,0 +1,28 @@
+function calcularIdade() {
+    const anoNascimento = document.getElementById('ano_nascimento').value;
+
+    resultado ="";
+
+    idade = 2026 - anoNascimento;
+
+    switch (true) {
+        case (anoNascimento >= 1900 && anoNascimento <= 1960):
+            resultado = "Voto não é obrigatório.";
+            break;
+        case (anoNascimento >= 1961 && anoNascimento <= 2008):
+            resultado = "Voto é obrigatório.";
+            break;
+
+        case (anoNascimento == 2009 || anoNascimento == 2010):
+            resultado = "Voto é opcional.";
+            break;
+
+        case (anoNascimento >= 2011 && anoNascimento <= 2026):
+            resultado = "Não pode votar";
+            break;
+        default:
+            resultado = "Ano de nascimento inválido.";
+    }
+
+    document.getElementById('resultado').textContent = resultado;
+}
