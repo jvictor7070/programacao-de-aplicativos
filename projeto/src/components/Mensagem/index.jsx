be65f9a40782073396c2167ec75009537d7498ff
jvictor7070 <joao.v.coutinho7@ba.estudante.senai.br>
2026-03-26 16:57:53 -0300

    reactjs

---
+++ projeto/src/components/Mensagem/index.jsx
@@ -0,0 +1,13 @@
+import '.style.css'
+
+function Mensagem() {
+  return (
+    // Comandos HTML
+    <div className='mensagem'>
+        <h1>Componentes</h1>
+        <p>Este é um componente cm uma mensagem simples</p>
+    </div>
+  )
+}
+
+export default Mensagem
