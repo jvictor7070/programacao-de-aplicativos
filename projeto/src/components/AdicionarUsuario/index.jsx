a9a369a12ace270fe00bde72dd9c040c13478b66
Aluno <Aluno@C302-LAB-000.senaiba.com.br>
2026-04-06 15:08:40 -0300

    Atividade: lista de usuario

---
+++ projeto/src/components/AdicionarUsuario/index.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import './style.css'
+
+export default function AdicionarUsuario() {
+    const [nome, setNome] = useStates('')
+    const [email, setEmail] = useStates('')
+    const [listaDeUsuarios, setListaDeUsuarios] = useStates([])
+
+    const handlerAdicionarUsuario = (event) => {
+        // Evitando que página recarregue ao mudar algum elemento na página. 
+        event.preventDefault()
+        // Verificando se os campos possuem algum conteúdo. 
+        if (nome && email) {
+            // Adicionar novo usuário
+            setListaDeUsuarios([...listaDeUsuarios, {nome, email}])
+            // Definindo os campos como vazio
+            setNome('')
+            setEmail('')
+        }
+    }
+
+    return(
+        // Criando Html da página
+        <div className='formulario'>
+            <h2>Adicionar usuário</h2> 
+            <form onSubmit={handlerAdicionarUsuario}>
+                <input
+                type="text"
+                value={nome}
+                onChange={(e) => setNome(e.target.value)}
+                />
+                <input
+                type="text"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                />
+                <button type="submit">Adicionar</button>
+                </form>
+                <hr />
+
+                <h2>Usuários Adicionados</h2>
+                <ul>
+                    {/* Usando o map para mostrar cada usuário na lista com base no índice.  */}
+                    {listaDeUsuarios.map((usuario, index) =>(
+                        <li key={index}>
+                            {usuario.nome} - {usuario.email}
+                        </li>
+                    ))}
+                </ul>
+                </div>
+    )
+}
